f782a8a921e6dae1180fbf75641cc29fbc991e5b
Aliakbar Rzayev <95173252+A1iakbar@users.noreply.github.com>
2026-01-21 02:18:52 +0000

    Add files via upload

---
+++ code/00_setup_packages.r
@@ -0,0 +1,43 @@
+# ============================================================
+# IJC437 - Introduction to Data Science
+# Script: 00_setup_packages.R
+#
+# Purpose:
+# - Install and load all required R packages for the project.
+# - Run once before executing the analysis scripts.
+#
+# ============================================================
+
+required_packages <- c(
+  "tidyverse",
+  "lubridate",
+  "httr",
+  "jsonlite",
+  "janitor",
+  "ggplot2",
+  "patchwork",
+  "zoo",
+  "caret",
+  "randomForest",
+  "xgboost",
+  "Metrics",
+  "dplyr",
+  "readr",
+  "purr",
+  "tidyr",
+  "tibble",
+  "scales",
+  "forecast",
+  "Hmisc",
+  "tidymodels",
+  "rsample",
+  "ranger",
+  "bonsai"
+)
+
+
+
+
+
+# Load packages
+invisible(lapply(required_packages, library, character.only = TRUE))
